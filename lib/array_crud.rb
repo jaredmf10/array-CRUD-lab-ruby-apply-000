656bb22207d000c8aa2df5fd8fd0da6c6989de98
Jared Fleming <jaredmf10@gmail.com>
2020-01-06 15:36:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -5,40 +5,72 @@ end
 
 # create an array with four elements
 def create_an_array
+<<<<<<< HEAD
  ["wow", "two", "am", "arrays!"]
+=======
+ ["one", "two", "three", "four"]
+>>>>>>> a27689138132856cb15c760a8ad8ef9abfdae0b7
 end
 
 # take arguments of an array and an element and adds that element to the end of the array
 def add_element_to_end_of_array(array, element)
+<<<<<<< HEAD
 create_an_array << "arrays!"
+=======
+
+>>>>>>> a27689138132856cb15c760a8ad8ef9abfdae0b7
 end
 
 # take arguments of an array and an element and adds that element to the start of the array
 def add_element_to_start_of_array(array, element)
+<<<<<<< HEAD
 create_an_array.unshift("wow")
+=======
+
+>>>>>>> a27689138132856cb15c760a8ad8ef9abfdae0b7
 end
 
 # takes in an argument of an array and removes the last element in the array
 def remove_element_from_end_of_array(array)
+<<<<<<< HEAD
 create_an_array.pop
+=======
+
+>>>>>>> a27689138132856cb15c760a8ad8ef9abfdae0b7
 end
 
 # take in an argument of an array and removes the first element from the array
 def remove_element_from_start_of_array(array)
+<<<<<<< HEAD
 create_an_array.shift
+=======
+
+>>>>>>> a27689138132856cb15c760a8ad8ef9abfdae0b7
 end
 
 # takes in an argument of an array and an index number and returns the element stored at that index
 def retrieve_element_from_index(array, index_number)
+<<<<<<< HEAD
 create_an_array[2]
+=======
+
+>>>>>>> a27689138132856cb15c760a8ad8ef9abfdae0b7
 end
 
 # takes in an argument of an array and returns the first element stored in the array
 def retrieve_first_element_from_array(array)
+<<<<<<< HEAD
 create_an_array[0]
+=======
+
+>>>>>>> a27689138132856cb15c760a8ad8ef9abfdae0b7
 end
 
 # takes in an argument of an array and returns the last element of that array
 def retrieve_last_element_from_array(array)
+<<<<<<< HEAD
 create_an_array.last
+=======
+
+>>>>>>> a27689138132856cb15c760a8ad8ef9abfdae0b7
 end
